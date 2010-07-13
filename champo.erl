5e8201ec739f4d41f239058d1dfcfcea6653f24e
Olivier Girondel <olivier@biniou.info>
2010-07-13 05:39:15 +0200

    Pre new-ETS version

--- champo.erl
+++ champo.erl
@@ -43,10 +43,15 @@
 %% -export([chrom/0, chrom/2]).
 
 %% GA parameters
--define(POP_SIZE, 20). %%16). %% 200). %%200000).
+-define(POP_SIZE, 10000). %%16). %% 200). %%200000).
+
+%% Bench: version avant ETS en //:
+%% POP=10000 -> s/run
+%% Apres:
+%% POP=10000 -> s/run
 
 %% Mutations
--define(P_MUTATION, 5). %%1000). %% 1 chance sur 1000
+-define(P_MUTATION, 100). %%1000). %% 1 chance sur 1000
 -define(NB_MUTATIONS, 6).
 
 %% CPU cooling pauses
