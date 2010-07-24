9c1d4f282faf6e4bad2426529752b5cbe5589841
Olivier Girondel <olivier@biniou.info>
2010-07-24 20:01:49 +0200

    Added chrom module

---
+++ chrom.erl
@@ -0,0 +1,17 @@
+-module(chrom).
+-author('olivier@biniou.info').
+
+-compile([export_all]). %% easy debugging
+-export([get/1, display/1]).
+
+-define(TODO, 'TODO').
+
+
+get(_Pid) ->
+    ?TODO.
+
+
+display(_Pid) ->
+    ?TODO.
+
+
